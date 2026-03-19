54c59b91d754b8dcdfa3efde5c760a664c54ef96
Luis Passos <luis.passos@datacentric-tech.com>
2026-03-19 14:57:41 +0000

    feat: pass host cwd for path normalization

--- presentation-utils.ps1
+++ presentation-utils.ps1
@@ -1,5 +1,6 @@
 
 docker run --rm `
+    -e "PU_HOST_PWD=$($PWD.Path)" `
     -v "${PWD}:/work" `
     -v "${HOME}/Documents/asciidoctor_styles:/themes/asciidoctor:ro" `
     -v "${HOME}/Documents/marp_styles:/themes/marp:ro" `
--- presentation-utils.sh
+++ presentation-utils.sh
@@ -3,6 +3,7 @@
 set -euo pipefail
 
 docker run --rm \
+  -e PU_HOST_PWD="$PWD" \
   -v "${PWD}:/work" \
   -v "${HOME}/Documents/asciidoctor_styles:/themes/asciidoctor:ro" \
   -v "${HOME}/Documents/marp_styles:/themes/marp:ro" \
