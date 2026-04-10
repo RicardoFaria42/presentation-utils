9e3e729b7fb2c298db1aa80444cb73ef490c6208
Luis Passos <luis.passos@datacentric-tech.com>
2026-04-10 08:54:04 +0100

    Add resolve_drawio_path helper to drawio-image plugin

--- plugins/asciidoctor/drawio-image/extension.rb
+++ plugins/asciidoctor/drawio-image/extension.rb
@@ -11,6 +11,26 @@ module PresentationUtils
       def process(_document)
         nil
       end
+
+      private
+
+      # Resolves the absolute path of a .drawio source file.
+      # The target is resolved relative to imagesdir (which itself is relative to docdir
+      # when it is not an absolute path).
+      def resolve_drawio_path(doc, target)
+        docdir    = doc.attr('docdir', Dir.pwd).to_s
+        imagesdir = doc.attr('imagesdir', '').to_s
+
+        base = if imagesdir.empty?
+                 docdir
+               elsif File.absolute_path?(imagesdir)
+                 imagesdir
+               else
+                 File.expand_path(imagesdir, docdir)
+               end
+
+        File.expand_path(target, base)
+      end
     end
   end
 end
